0c49af3ca9ceea31cd2680b40e4726eec32c5e07
Xuehua-Zhou <genlies@126.com>
2021-03-15 08:27:56 +0800

    Copy audio config files
    Fix the issue that audio configs be removed in commit 6d4ccd3

--- device/config-files.mk
+++ device/config-files.mk
@@ -1,5 +1,7 @@
-### Modem Configs
+### Audio and Modem Configs
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/config/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
+    $(DEVICE_PATH)/config/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(DEVICE_PATH)/config/modem/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/modem.conf \
     $(DEVICE_PATH)/config/modem/54/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/54/modem.conf \
     $(DEVICE_PATH)/config/modem/55/modem.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/customization/modem/55/modem.conf \
